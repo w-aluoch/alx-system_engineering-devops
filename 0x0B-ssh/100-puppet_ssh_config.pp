a57ed9f02efdf773d8f66fb754ec4390e8bbf497
w-aluoch <wendyaluoch96@gmail.com>
2024-05-08 11:34:38 +0000

    ssh

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -2,5 +2,5 @@
 
 exec {'Puppet Configuration':
   path    => '/usr/bin:/bin',
-  command => 'echo -e "Host 54.237.9.122\n	IdentityFile ~/.ssh/school\n	PasswordAuthentication no" >> /etc/ssh/ssh_config',
+  command => 'echo -e "Host 34.203.77.57\n	IdentityFile ~/.ssh/school\n	PasswordAuthentication no" >> /etc/ssh/ssh_config',
 }
