4e166c9ab6ca87d39eb40dbeb3e55c4f835d5c05
w-aluoch <wendyaluoch96@gmail.com>
2024-05-16 09:03:58 +0000

    0-the_sky_is_the_limit_not.pp

---
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -0,0 +1,12 @@
+# fixing requests limit at ngix
+
+exec { 'fix--for-nginx':
+	command => 'sed -i "s/15/4096/" /etc/default/nginx',
+	path    => '/usr/local/bin/:/bin/'
+}
+
+# Restart Nginx
+	-> exec { 'nginx-restart':
+ 	command => 'nginx restart',
+	 path    => '/etc/init.d/'
+}
